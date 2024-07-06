62dd7cb182d05b2d61f8a1c41bee4c102dee2484
Miguel Bernier <miguelbernier89@gmail.com>
2024-07-05 20:04:18 -0500

    nuevo

--- sistem_vent/sistema_ventas/settings.py
+++ sistem_vent/sistema_ventas/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'base1',
         'USER': 'root',
-        'PASSWORD': 'mb240405',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
